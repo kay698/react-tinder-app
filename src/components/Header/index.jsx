9c283582dd94c9ae5877e9f91feaee6d4b877754
kel <ogbukakelechi20@gmail.com>
2021-02-04 15:43:30 +0100

    header complete

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import PersonIcon from "@material-ui/icons/Person";
 import IconButton from "@material-ui/core/IconButton";
+import "./styles.css";
 
 const Header = () => {
   return (
-    <div>
+    <div  className="header_cover">
       <IconButton>
         <PersonIcon fontSize="large" className="header_icon" />
       </IconButton>
+      <img src={require("../../assets/tinder.png")} alt="tinder_logo" className="logo"/>
     </div>
   );
 };
